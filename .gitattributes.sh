313837b5960685f4574495d143ae75ac597b8cb8
Somashekarbm <1dt21ai008@dsatm.edu.in>
2024-06-22 19:04:19 +0530

    empty

---
+++ .gitattributes.sh
@@ -0,0 +1,9 @@
+# Ensure consistent line endings
+* text=auto
+
+# Handle line endings for specific types of files
+*.js text eol=lf
+*.ts text eol=lf
+*.md text eol=lf
+*.txt text eol=lf
+*.json text eol=lf
