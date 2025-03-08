5b977970148b736b1073cca0f521bfd4a7fabf70
BM SOMASHEKAR <121337385+Somashekarbm@users.noreply.github.com>
2025-03-08 12:56:03 +0530

    Update SearchMovies.js

--- React-Projects/MovieSearchApp/SearchMovies.js
+++ React-Projects/MovieSearchApp/SearchMovies.js
@@ -10,7 +10,7 @@ export default function SearchMovies(){
     const searchMovies = async (e) => {
         e.preventDefault();
                 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
